130e7fec703d9940fb44dec3a7b257a0432154b2
Evan <oakeef@gmail.com>
2016-12-09 12:16:40 -0400

    Integrated API to play youtube movie trailer
    Created the YoutubePLayerView,
    created the PlaybackEventListener and PlayerStateChangeListener.
    
    Created an API Key on the google website.
    
    The app takes the API key and the Video ID (The last part of the youtube
    URL after the last = character)

--- app/src/main/java/com/example/evan/youtubeapitest/MainActivity.java
+++ app/src/main/java/com/example/evan/youtubeapitest/MainActivity.java
@@ -1,13 +1,105 @@
 package com.example.evan.youtubeapitest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    public static final String API_KEY = "AIzaSyAWlv2SpKUdbtK7sPku-rBpCJQPcm7BAFE";
+    public static final String VIDEO_ID = "3KWAxBlZKMA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initalizing Youtube PLayer View
+        YouTubePlayerView youtubePlayerView = (YouTubePlayerView) findViewById(R.id.youtube_player);
+        youtubePlayerView.initialize(API_KEY, this);
     }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        Toast.makeText(this, "Failure to Initialize!", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player, boolean wasRestored) {
+
+        //add listeners to YouTubePlayer instance
+        player.setPlayerStateChangeListener(playerStateChangeListener);
+        player.setPlaybackEventListener(playbackEventListener);
+
+        //start buffering
+        if(!wasRestored) {
+            player.cueVideo(VIDEO_ID);
+        }
+    }
+    //create the PlaybackEventListener
+    private YouTubePlayer.PlaybackEventListener playbackEventListener = new YouTubePlayer.PlaybackEventListener() {
+        @Override
+        public void onPlaying() {
+
+        }
+
+        @Override
+        public void onPaused() {
+
+        }
+
+        @Override
+        public void onStopped() {
+
+        }
+
+        @Override
+        public void onBuffering(boolean b) {
+
+        }
+
+        @Override
+        public void onSeekTo(int i) {
+
+        }
+    };
+
+    //
+    private YouTubePlayer.PlayerStateChangeListener playerStateChangeListener = new YouTubePlayer.PlayerStateChangeListener() {
+        @Override
+        public void onLoading() {
+
+        }
+
+        @Override
+        public void onLoaded(String s) {
+
+        }
+
+        @Override
+        public void onAdStarted() {
+
+        }
+
+        @Override
+        public void onVideoStarted() {
+
+        }
+
+        @Override
+        public void onVideoEnded() {
+
+        }
+
+        @Override
+        public void onError(YouTubePlayer.ErrorReason errorReason) {
+
+        }
+    };
+
+
 }
